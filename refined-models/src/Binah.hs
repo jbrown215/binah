93cfec58c320340d857bd4e861bd62a9ff235e31
Jordan Brown <jmbrown@andrew.cmu.edu>
2017-10-11 18:22:53 -0400

    Add command line options for version and help

--- refined-models/src/Binah.hs
+++ refined-models/src/Binah.hs
@@ -1,4 +1,36 @@
 module Binah where
 
+import Control.Monad
+import Data.Char
+import Data.List
+import System.Console.GetOpt
+import System.Environment
+import System.Exit
+import System.IO
+import Text.Printf
+
+
+data Flag =
+      Version    -- -v
+    | Help       -- --help
+    deriving (Eq, Ord, Enum, Show, Bounded)
+
+version = "0.0"
+help = "*TODO*"
+
+flags =
+    [ Option ['v'] ["version"] (NoArg Version)
+        "Prints the version number."
+    , Option ['h'] ["help"] (NoArg Help)
+        "Prints the help page."
+    ]
+
+parse argv = case getOpt Permute flags argv of
+    ([Version], _, []) -> putStrLn $ "Binah version " ++ version
+    ([Help], _, []) -> putStrLn help 
+    _ -> putStrLn "Usage is: TODO"
+
 run :: IO ()
-run = putStrLn "Binah version 0.0.0.0.0..."
+run = do
+    as <- getArgs
+    parse as
