f985314794fc42e6d3e6806c92c8a672bbdec30c
Jeanne Luning Prak <jluningp@andrew.cmu.edu>
2017-10-11 15:45:58 -0400

    parser and models/spec file generator

---
+++ refined-models/models.hs
@@ -0,0 +1,106 @@
+module Models where
+
+{-# LANGUAGE OverloadedStrings #-}
+import System.IO
+import Control.Monad
+import qualified Data.List.Split as Split
+import Data.List
+import Data.Char (isSpace, toUpper, toLower)
+import qualified Data.Set as Set
+import ModelsParser
+
+makeRevGroups :: [Stmt] -> [Stmt] -> [[Stmt]]
+makeRevGroups [] acc = [acc]
+makeRevGroups (x:xs) acc = case x of
+                             NewRecord _ -> acc : (makeRevGroups xs [x])
+                             Field _ _ -> makeRevGroups xs (x:acc)
+                             Deriving _ -> makeRevGroups xs (x:acc)
+
+makeGroups :: [Stmt] -> [[Stmt]]
+makeGroups l = filter (not . null) . (map (Data.List.reverse)) $ makeRevGroups l []
+
+prettyPrintStmt :: Stmt -> String
+prettyPrintStmt (NewRecord str) = str ++ "\n"
+prettyPrintStmt (Field var (Simple typ)) = "   " ++ var ++ " " ++ typ ++ "\n"
+prettyPrintStmt (Field var (Refined _ typ _)) = "   " ++  var ++ " " ++ typ ++ "\n"
+prettyPrintStmt (Deriving str) = "   deriving " ++ str ++ "\n"
+
+firstCapital :: String -> String
+firstCapital (s : ss) = toUpper s : ss
+
+sentenceCase :: String -> String
+sentenceCase str = firstCapital $ map toLower str
+
+formatRecordEntry :: String -> Stmt -> (String, String) 
+formatRecordEntry tableName e =
+  case e of
+    Field v (Simple t) -> (tableName ++ (firstCapital v) ++ " :: " ++ t,
+                           (tableName ++ (firstCapital v)))
+    Field v (Refined tv t refinement) ->
+      (tableName ++ (firstCapital v) ++ " :: {" ++ tv ++ ":" ++ t ++ " | " ++ (intercalate " " refinement) ++ "}",
+        (tableName ++ (firstCapital v)))
+      
+notDeriving :: Stmt -> Bool
+notDeriving (Deriving _) = False
+notDeriving _ = True
+    
+formatTableRecord :: [Stmt] -> (String, Set.Set String)
+formatTableRecord (NewRecord tableName : entries) =
+  let (formattedEntries, records) = unzip $
+                                    map (formatRecordEntry $ map toLower tableName)
+                                    (filter notDeriving entries) in
+  ("{-@\n" ++
+  "data " ++
+  (sentenceCase tableName) ++
+  " = \n\t{ " ++
+  (intercalate "\n\t, " formattedEntries) ++
+  "\n\t}" ++
+  "\n@-}",
+   Set.fromList records)
+
+needsTableName :: Set.Set String -> [String] -> Bool
+needsTableName s l = any (\x -> Set.member x s) l
+
+updateRefinement :: Set.Set String -> [String] -> [String]
+updateRefinement s l =
+  map (\x -> if Set.member x s then x ++ " t" else x) l
+
+formatDataEntry :: Set.Set String -> String -> Stmt -> String 
+formatDataEntry entrySet tableName e =
+  case e of
+    Field v (Simple t) -> tableName ++ (firstCapital v) ++ " :: " ++ "EntityField " ++ tableName ++ " " ++ t
+    Field v (Refined tv t refinement) -> tableName ++ (firstCapital v) ++ " :: " ++ "EntityField " ++
+                                         (if needsTableName entrySet refinement then "t:" ++ tableName
+                                         else tableName) ++
+                                         " {" ++ tv ++ ":" ++ t ++ " | " ++
+                                         (intercalate " " (updateRefinement entrySet refinement)) ++ "}"
+
+                                         
+formatTableData :: Set.Set String -> [Stmt] -> String
+formatTableData entrySet (NewRecord tableName : entries) =
+  "{-@\n" ++
+  "data EntityField " ++
+  (sentenceCase tableName) ++
+  " typ where \n   " ++
+  (intercalate "\n | " $ map (formatDataEntry entrySet (sentenceCase tableName)) entries) 
+  ++ "\n@-}"
+
+
+makeSpecType :: [Stmt] -> String
+makeSpecType stmts =
+  let (record, entries) = formatTableRecord stmts in
+    let tableData = (formatTableData entries) . (filter notDeriving) $ stmts in
+      record ++ "\n\n" ++ tableData
+
+makeSpecFile :: [Stmt] -> String
+makeSpecFile stmts = let groups = makeGroups stmts in 
+                          intercalate "\n\n" (map makeSpecType groups)
+
+makeModelsAndSpecFile :: String -> IO ()
+makeModelsAndSpecFile file = do stmts <- parseFile file
+                                simpleModels <- return $ concat (map prettyPrintStmt stmts)
+                                specs <- return $ makeSpecFile stmts
+                                writeFile (file ++ "simple-models") simpleModels
+                                writeFile (file ++ ".spec") specs
+                                return ()
+
---
+++ refined-models/parser.hs
@@ -0,0 +1,112 @@
+module ModelsParser where
+import System.IO
+import Control.Monad
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token as Token
+import qualified Data.List.Split as Split
+
+{- Table Datatype -}
+type Var = String
+type SimpleType = String
+
+{- Predicate is just a list of strings right now -}
+data Type = Refined Var SimpleType [String]
+          | Simple SimpleType
+          deriving (Show)
+
+data Stmt = NewRecord Var
+          | Field Var Type
+          | Deriving Var
+          deriving (Show)
+
+{- Refined Models Language -}
+languageDef =
+  emptyDef { Token.commentStart   = ""
+           , Token.commentEnd = ""
+           , Token.commentLine    = ""
+           , Token.nestedComments = True
+           , Token.identStart     = letter 
+           , Token.identLetter    = alphaNum <|> oneOf "_'"
+           , Token.opStart        = alphaNum <|> oneOf "_'" <|> oneOf "!#$%&*+./<=>?@\\^-~"
+           , Token.opLetter       = alphaNum <|> oneOf "_'" <|> oneOf "!#$%&*+./<=>?@\\^-~"
+           , Token.reservedOpNames= [":", "|"]
+           , Token.reservedNames  = ["deriving"]
+           , Token.caseSensitive  = True
+           }
+
+{- Lexer Helpers -}
+lexer = Token.makeTokenParser languageDef
+parens = Token.parens lexer
+braces = Token.braces lexer
+reservedOp = Token.reservedOp lexer
+reservedName = Token.reserved lexer
+identifier = Token.identifier lexer
+whiteSpace = Token.whiteSpace lexer
+otherOp = Token.operator lexer
+typeOf = Token.colon lexer
+
+{- Helpers for Parsing Refined Types -}
+predicateString :: Parser Var
+predicateString = otherOp <|> identifier
+
+predicateSequence :: Parser [Var]
+predicateSequence = do list <- (sepBy1 predicateString whiteSpace)
+                       return list
+
+refinedtype :: Parser Type
+refinedtype =
+  do var <- identifier
+     typeOf
+     simptype <- identifier
+     reservedOp "|"
+     predicate <- predicateSequence
+     return $ Refined var simptype predicate
+
+{- Helpers for Parsing Types -}
+typ :: Parser Type
+typ = (braces refinedtype)
+        <|> simpletype 
+
+simpletype :: Parser Type
+simpletype = do typ <- identifier
+                return $ Simple typ
+                
+{- Helpers for Parsing Derive Statements -}
+derive :: Parser Stmt
+derive =
+  do reservedName "deriving"
+     var <- parens identifier <|> identifier
+     return $ Deriving var
+
+{- Helpers for Parsing Table and Fields -}
+field :: Parser Stmt
+field = do var <- identifier
+           t <- typ
+           return $ Field var t
+
+newtable :: Parser Stmt
+newtable = do var <- identifier
+              return $ NewRecord var
+
+declaration :: Parser Stmt
+declaration = try field <|> newtable <|> derive
+
+{- Parsing -}
+whileParser :: Parser Stmt
+whileParser = whiteSpace >> declaration
+
+parseString :: String -> Stmt
+parseString str =
+  case parse whileParser "" str of
+    Left e  -> error $ show e
+    Right r -> r
+   
+parseExprs :: [String] -> [Stmt]
+parseExprs = (map parseString) 
+
+parseFile :: String -> IO [Stmt]
+parseFile file = do program  <- readFile file
+                    lines <- return $ Split.splitOn "\n" program
+                    return $ parseExprs lines
