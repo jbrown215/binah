1f7806fef056fa56475712037aeb20c943ed19a8
Jordan Brown <jmbrown@andrew.cmu.edu>
2017-10-27 02:06:39 -0400

    Liquid Haskell bug?

--- db/world/World.hs
+++ db/world/World.hs
@@ -8,14 +8,18 @@ module World where
 
 data World
 
-{-@ data Blob = Blob {xVal :: {v:Int | v == 3}, yVal :: Int} @-}
+{-@ data Blob = Blob {xVal :: {v:Int | v = 3}, yVal :: Int} @-}
 data Blob = Blob {xVal :: Int, yVal :: Int}
 
 
-{-@ measure cannonicalBlob:: Blob @-}
+{-@ measure cannonicalBlob :: Blob @-}
 
-{-@ data Range = Range {lower :: {v:Int | v == (xVal cannonicalBlob)}, upper :: Int} @-}
+{-@ data Range = Range {lower :: {v:Int | v = (xVal cannonicalBlob)}, upper :: Int} @-}
 data Range = Range {lower :: Int, upper :: Int}
 
-someRange :: () -> Range
-someRange () = Range {lower = 3, upper = 4}
+{-@ makeBlob :: () -> {b:Blob | xVal b == (xVal cannonicalBlob)} @-}
+makeBlob :: () -> Blob
+makeBlob () = Blob {xVal = 3, yVal = 4}
+
+makeRange :: () -> Range
+makeRange () = Range {lower = 3, upper = 4}
