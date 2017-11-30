fc1828da702a6d5a67d917080cd265ce5ae8464b
Jordan Brown <jmbrown@andrew.cmu.edu>
2017-11-29 21:12:43 -0500

    Verify queries with multiple filters!

--- db/EvalQuery.hs
+++ db/EvalQuery.hs
@@ -50,6 +50,18 @@ instance PersistEntity Blob where
         BlobXVal :: EntityField Blob Int
         BlobYVal :: EntityField Blob Int
 
+{-@ data Range = Range { lower :: Int, upper :: Int } @-}
+data Range = Range { lower :: Int, upper :: Int }
+
+instance PersistEntity Range where
+    {-@ data EntityField record typ where
+        RangeLower :: EntityField Range Int
+      | RangeUpper :: EntityField Range Int
+    @-}
+    data EntityField Range typ where
+        RangeLower :: EntityField Range Int
+        RangeUpper :: EntityField Range Int
+
 {-@ filter :: f:(a -> Bool) -> [a] -> [{v:a | f v}] @-}
 filter :: (a -> Bool) -> [a] -> [a]
 filter f (x:xs)
@@ -75,13 +87,56 @@ evalQBlob filter blob = case filterField filter of
     BlobXVal -> evalQBlobXVal (filterFilter filter) (filterValue filter) (xVal blob)
     BlobYVal -> evalQBlobYVal (filterFilter filter) (filterValue filter) (yVal blob)
 
+{-@ reflect evalQsBlob @-}
+evalQsBlob :: [Filter Blob typ] -> Blob -> Bool
+evalQsBlob (f:fs) blob = evalQBlob f blob && (evalQsBlob fs blob)
+evalQsBlob [] _ = True
+
+
 {-@ filterQBlob :: f:(Filter Blob a) -> [Blob] -> [{b:Blob | evalQBlob f b}] @-}
 filterQBlob :: Filter Blob a -> [Blob] -> [Blob]
 filterQBlob q = filter (evalQBlob q)
 
-{-@ assume select :: f:(Filter Blob a) -> [{b:Blob | evalQBlob f b}] @-}
-select :: Filter Blob a -> [Blob]
-select _ = undefined
+{-@ filterQsBlob :: f:[(Filter Blob a)] -> [Blob] -> [{b:Blob | evalQsBlob f b}] @-}
+filterQsBlob :: [Filter Blob a] -> [Blob] -> [Blob]
+filterQsBlob qs = filter (evalQsBlob qs)
+
+{-@ reflect evalQRangeLower @-}
+evalQRangeLower :: PersistFilter -> Int -> Int -> Bool
+evalQRangeLower EQUAL filter given = filter == given
+evalQRangeLower LE filter given = given <= filter
+evalQRangeLower GE filter given = given >= filter
+
+{-@ reflect evalQRangeUpper @-}
+evalQRangeUpper :: PersistFilter -> Int -> Int -> Bool
+evalQRangeUpper EQUAL filter given = filter == given
+evalQRangeUpper LE filter given = given <= filter
+evalQRangeUpper GE filter given = given >= filter
+
+{-@ reflect evalQRange @-}
+evalQRange :: Filter Range typ -> Range -> Bool
+evalQRange filter range = case filterField filter of
+    RangeLower -> evalQRangeLower (filterFilter filter) (filterValue filter) (lower range)
+    RangeUpper -> evalQRangeUpper (filterFilter filter) (filterValue filter) (upper range)
+
+{-@ filterQRange :: f:(Filter Range a) -> [Range] -> [{b:Range | evalQRange f b}] @-}
+filterQRange :: Filter Range a -> [Range] -> [Range]
+filterQRange q = filter (evalQRange q)
+
+
+-- select functions
+
+{-@ assume selectBlob :: f:[(Filter Blob a)] -> [{b:Blob | evalQsBlob f b}] @-}
+selectBlob :: [Filter Blob a] -> [Blob]
+selectBlob _ = undefined
+
+{-@ assume selectRange :: f:(Filter Range a) -> [{b:Range | evalQRange f b}] @-}
+selectRange :: Filter Range a -> [Range]
+selectRange _ = undefined
+
+data Table a where
+    BlobTable :: Table Blob
+    RangeTable :: Table Range
 
 -- Client code:
 
@@ -89,20 +144,42 @@ select _ = undefined
 -- Why does only the second one typecheck?
 -- {-@ getZeros :: () -> [{b:Blob | xVal b = 0}] @-}
 -- getZeros :: () -> [Blob]
--- getZeros () = select (createEqQuery BlobXVal 0)
+-- getZeros () = selectBlob (createEqQuery BlobXVal 0)
 
 {-@ getZeros_ :: () -> [{b:Blob | xVal b = 0}] @-}
 getZeros_ :: () -> [Blob]
-getZeros_ () = select (Filter BlobXVal 0 EQUAL)
+getZeros_ () = selectBlob [(Filter BlobXVal 0 EQUAL)]
 
 {-@ getBiggerThan10 :: () -> [{b:Blob | xVal b >= 10}] @-}
 getBiggerThan10 :: () -> [Blob]
-getBiggerThan10 () = select (Filter BlobXVal 11 GE)
+getBiggerThan10 () = selectBlob [(Filter BlobXVal 11 GE)]
+
+{-@ getInRange :: () -> [{b:Blob | xVal b >= 10  && xVal b <= 20 && yVal b >= 0 && yVal b <= 11}] @-}
+getInRange :: () -> [Blob]
+getInRange () = selectBlob [ (Filter BlobXVal 10 GE)
+                           , (Filter BlobXVal 20 LE)
+                           , (Filter BlobYVal 0 GE)
+                           , (Filter BlobYVal 11 LE)
+                           ]
 
 -- Should not typecheck
-{-@ getBiggerThan10Fail :: () -> [{b:Blob | xVal b >= 10}] @-}
-getBiggerThan10Fail :: () -> [Blob]
-getBiggerThan10Fail () = select (Filter BlobXVal 9 GE)
+-- {-@ getBiggerThan10Fail :: () -> [{b:Blob | xVal b >= 10}] @-}
+-- getBiggerThan10Fail :: () -> [Blob]
+-- getBiggerThan10Fail () = selectBlob (Filter BlobXVal 9 GE)
+--
+-- {-@ getInRange_ :: () -> [{b:Blob | xVal b >= 10  && xVal b <= 19 && yVal b >= 0 && yVal b <= 11}] @-}
+-- getInRange_ :: () -> [Blob]
+-- getInRange_ () = selectBlob [ (Filter BlobXVal 10 GE)
+--                             , (Filter BlobXVal 20 LE)
+--                             , (Filter BlobYVal 0 GE)
+--                             , (Filter BlobYVal 11 LE)
+--                             ]
 
--- coming up: selectFrom :: Table -> Filter -> Blob
--- finally: selectWhere :: Table -> [Filter] -> Blob! We're almost there!
+
+-- It would be nice if we could parameterize things by a table instead of
+-- having separate select functions for each table.
+-- {-@ reflect evalQ @-}
+-- evalQ :: Table a -> Filter a typ -> a -> Bool
+-- evalQ table filter rec = case table of
+--     BlobTable -> evalQBlob filter rec
+--     RangeTable -> evalQRange filter rec
