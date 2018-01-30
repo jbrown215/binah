771990df38e543e2e9db9dfa70df18391101d5aa
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-01-30 10:22:27 -0800

    whew. this works

--- db-lib/src/Models.hs
+++ db-lib/src/Models.hs
@@ -19,31 +19,13 @@ import           Database.Persist
 import           Database.Persist.Sqlite
 import           Database.Persist.TH
 
-{- data EntityField Blob typ where
-      BlobXVal :: EntityField Blob {v:_ | True}
-    | BlobYVal :: EntityField Blob {v:_ | True}
-    | BlobId   :: EntityField Blob {v:_ | True}
-  -}
+{-@ data EntityField Blob typ where
+      Models.BlobXVal :: EntityField Blob {v:_ | True}
+    | Models.BlobYVal :: EntityField Blob {v:_ | True}
+    | Models.BlobId   :: EntityField Blob {v:_ | True}
+  @-}
 
 {-@ assume Prelude.error :: String -> a @-} 
-
-{- 
-data RefinedPersistFilter = EQUAL | LE | GE
-
-data RefinedFilter record typ = RefinedFilter
-    { refinedFilterField  :: EntityField record typ
-    , refinedFilterValue  :: typ
-    , refinedFilterFilter :: RefinedPersistFilter
-    } 
-
-{-@ reflect evalQBlob @-}
-evalQBlob :: RefinedFilter Blob typ -> Blob -> Bool
-evalQBlob filter blob = case refinedFilterField filter of
-  BlobXVal -> True
-  BlobYVal -> True
-  BlobId ->   True
-	
--}
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 Person
